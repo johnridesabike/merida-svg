d32e45ec5d248c0c9e4766b2c478fe06858f14ac
John <37978984+johnridesabike@users.noreply.github.com>
2019-06-26 22:00:16 -0400

    baked the transforms into the paths

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const path = require("path");
-const {writeFile} = require("fs");
+"use strict";
 const openType = require("opentype.js");
+const path = require("path");
+const {pipe} = require("ramda");
+const svgPath = require("svgpath");
+const {writeFile} = require("fs");
 
 // This separates all of the different sub-paths to their own lines, which can
 // make text-editing the SVG much easier.
-const formatPath = ({path, indent = ""}) => (
+const formatPath = (indent) => (path) => (
     path.replace(/Z/g, "Z\n" + indent).trim()
 );
 
-const svgTemplate = (path) => `<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+const path2SVG = (path) => `<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
 <!-- This file was auto-generated. Copy your changes to a new file. -->
 <svg
     width="512"
@@ -16,32 +19,36 @@ const svgTemplate = (path) => `<?xml version="1.0" encoding="UTF-8" standalone="
     viewBox="0 0 2048 2048"
     xmlns="http://www.w3.org/2000/svg"
 >
-<g transform="scale(1, -1) translate(0, -2048)">
-    <path
-        d="
-            ${formatPath({path, indent: "            "})}
-        "
-    />
-</g>
+<path
+    d="
+        ${path}
+    "
+/>
 </svg>
 `;
 
 function outDir(fileName) {
-    return path.join(__dirname, "svg", fileName)
+    return path.join(__dirname, "svg", fileName);
 }
 
 function writeGlyphs(fontData) {
-    return function writeGlyphsFromData([name, number]) {
-        const path = merida.glyphs.get(number).path.toPathData();
-        const svg = svgTemplate(path);
+    const id2Svg = pipe(
+        (number) => fontData.glyphs.get(number).path.toPathData(),
+        // Bake the transformations into the path. We're never going to need
+        // the pieces upside down or outside the bounding area.
+        (data) => svgPath(data).scale(1, -1).translate(0, 2048).toString(),
+        formatPath("        "),
+        path2SVG
+    );
+    return function writeGlyphsFromData([name, id]) {
         const fileName = outDir(name + ".svg");
         function logger(err) {
             if (err) {
-                throw err
-            };
-            console.log("Generated: " + fileName)
+                throw err;
+            }
+            console.log("Generated: " + fileName);
         }
-        writeFile(fileName, svg, logger)
+        writeFile(fileName, id2Svg(id), logger);
     };
 }
 
@@ -61,5 +68,5 @@ const glyphs = {
     blackKing: 19
 };
 const filePath = path.join(__dirname, "merid_tt", "MERIFONT.TTF");
-const merida = openType.loadSync(filePath)
+const merida = openType.loadSync(filePath);
 Object.entries(glyphs).forEach(writeGlyphs(merida));
